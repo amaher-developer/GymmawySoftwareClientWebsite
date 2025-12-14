b3d1137671bc8d06db4c6517f4efbd53abad9027
maher <amaher.developer@gmail.com>
2025-12-14 14:22:48 +0200

    Add GenericController class with initial structure

---
+++ Modules/Zonegym/app/Http/Controllers/Front/GenericController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Modules\Zonegym\app\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+class GenericController extends Controller
+{
+
+
+    public function __construct()
+    {
+
+    }
+
+}
