125961b5c549f6e837e0f215e1d796375e89fd47
maher <amaher.developer@gmail.com>
2026-08-06 19:40:33 +0300

    fixt issue of images show

--- Modules/Sixtyminutes/app/Models/PTSubscription.php
+++ Modules/Sixtyminutes/app/Models/PTSubscription.php
@@ -27,7 +27,7 @@ class PTSubscription extends GenericModel
     {
         $image = $this->getRawOriginal('image');
         if($image)
-            return asset(self::$uploads_path.$image);
+            return env('APP_URL_MASTER').'uploads/pt-subscriptions/'.$image;
 
         return asset('resources/assets/front/img/preview_icon.png');
     }
